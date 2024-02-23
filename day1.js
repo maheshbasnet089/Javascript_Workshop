9087d70f6e1972d0ae6b795a2537c10d63ed27b8
maheshbasnet089 <basnetmanish089@gmail.com>
2024-02-23 18:28:41 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,44 @@
+
+// var cup = "Coffee"
+
+// let cup = "Coffee"
+// let cup = "Coffee"
+// cup = "Water"
+
+//  const secret = "hahathisissecret"  
+
+
+// if(true){
+//     const name = "John Doe"
+//     console.log(name," This is first One")
+// }
+
+// console.log(name," This is second one")
+
+// const age = 21
+// const isNepali = false
+
+// let name = "Manish"
+// name =123123
+
+// console.log(name  +  10)
+
+// let friend1 = "Pranjal"
+// let friend2 = "Bob"
+// let friend3 = "Anup"
+
+// let khoyo = ['DigitalPathshala',999,]
+// console.log(friends[2])\
+
+let khoyo = {
+    instituteName : "Digital Pathshala",
+    reactCoursePrice : 999
+}
+console.log(khoyo.instituteName)
+// key:value
+
+
+
+
+
+
