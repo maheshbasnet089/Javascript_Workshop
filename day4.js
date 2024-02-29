45aac1d431c7bbd6811aca8cf978ae2583f4968b
maheshbasnet089 <basnetmanish089@gmail.com>
2024-02-29 18:45:27 +0545

    learn about function,loops

---
+++ day4.js
@@ -0,0 +1,112 @@
+
+
+// console.log(1+1)
+// console.log(1+2)
+// console.log(1+3)
+
+// regular function 
+// function add(a,b,c=99){
+//     console.log(a+b+c)
+// }
+
+
+// named function
+// let add = function(a,b,c){
+//     console.log(a+b+c)
+// }
+// add(1,3,1)
+
+// arrow function 
+
+// let add  = (a,b,c) => {
+//     console.log(a+b+c)
+//     console.log("Hello")
+// }
+// add(1,2,1)
+// function (a,b,c){
+//     console.log("Hello world")
+// }
+
+// const callBack = (fn)=>{
+//     return fn
+// }
+
+// const anonymous = ()=>{
+//     console.log("I am anonymous")
+// }
+
+// anonymous()
+
+// const square = (a) => a * a
+
+// const result = square(3)
+// console.log(result)
+
+
+
+
+// LOOPS in javascript 
+
+const friends = ['Manish','Bob','Pranjal',"Hello"]
+// console.log(friends[1])
+// console.log(friends[2])
+
+// for(let i=1; i< 11; i++){
+//     console.log(i)
+// }
+
+// for(initialization;condition;iteration)
+// i++ === i = i + 1
+
+// for(let i = 0 ; i < friends.length;i++){
+//     console.log(friends[i])
+// }
+
+
+// let i = 0 ; 
+// while(i<friends.length){
+//     console.log(friends[i])
+//     i++
+// }
+
+// for(haha of friends){
+//     console.log(haha)
+// }
+
+// const person = {
+//     name : "Manish Basnet",
+//     address : "Itahari"
+// }
+
+// for(let key in person){
+//     console.log(key,person[key])
+// }
+
+
+// console.log(name)
+// var name = "Manish Basnet"
+
+// let name = "Mahesh Basnet"
+
+
+// add()
+// function add(){
+//     console.log(1+1)    
+// }
+
+
+// add()
+// // let add = ()=>{
+// //     console.log(1+1)
+// // }
+
+// var add = function(){
+//     console.log(1+1)
+// }
+
+
+function example(){
+    console.log(y)
+    let y = 99
+}
+example()
